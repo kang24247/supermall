c5c2125e6920610b69acd0280a1d3c96e0e81f88
kangyu <kangy8371@gmail.com>
2021-06-25 10:44:10 +0800

    绿色 先提交一次

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tabbar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:'TabBar',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+   #tabbar{
+    display: flex;
+    background-color: #f6f6f6;
+    justify-content: space-around;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-shadow: 0 -1px rgba(100, 100, 100, .1); 
+    
+  }
+  
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name='item-icon'></slot></div>
+    <div v-else><slot name='item-icon-active'></slot></div>
+    <div :style="activeStyle"><slot name='item-text'></slot></div><!-- :class="{active:isActive}" -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem',
+  props: {
+    path:String,
+    activeColor:{
+      type:String,
+      default:'indianred' 
+    }
+  },
+  data () {
+    return {
+      // isActive:true
+    }
+  },
+  computed: {
+    isActive(){
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle(){
+      return this.isActive ? {color:this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick(){
+      this.$router.push(this.path)
+    }
+  }
+}
+</script>
+
+<style>
+ 
+ .tab-bar-item{
+    flex:1;
+    height: 49px;
+    text-align: center;
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+  }
+  .tab-bar-item .item-text{
+      font-size: 14px;
+  }
+  /* .active{
+    color: indianred;
+  } */
+</style>
